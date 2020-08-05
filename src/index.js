8b2e48d7b3e9031691613e65f9a0a36e7791a727
zhangyanling77 <18783226594@163.com>
2020-08-05 17:46:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
+/* eslint-disable no-useless-concat */
+/* eslint-disable global-require */
+/* eslint-disable array-callback-return */
 const program = require('commander');
 const chalk = require('chalk');
 const { VERSION } = require('../lib/utils/constants');
 
-let actionMap = {
+const actionMap = {
   'create <app-name>': { // 配置命令的名字
     alias: '', // 别名
     description: 'create a new project', // 描述
     examples: [ // 使用示例
-      'daemon-cli create <app-name>'
-    ]
+      'daemon-cli create <app-name>',
+    ],
   },
   'init <app-name>': {
     alias: 'i',
     description: 'generate a project from a remote template',
     examples: [
-      'daemon-cli init <app-name>'
-    ]
+      'daemon-cli init <app-name>',
+    ],
   },
   'config [option]': {
     alias: 'c',
@@ -23,42 +26,42 @@ let actionMap = {
     examples: [
       'daemon-cli config set <key> <value>',
       'daemon-cli config get <key>',
-      'daemon-cli config unset <key>'
-    ]
+      'daemon-cli config unset <key>',
+    ],
   },
   '*': {
     alias: '',
     description: 'Unknown command',
-    examples: []
-  }
+    examples: [],
+  },
 };
 
-Object.keys(actionMap).map(action => {
-  let currentAction = actionMap[action];
+Object.keys(actionMap).map((action) => {
+  const currentAction = actionMap[action];
   program.command(action)
     .description(currentAction.description)
     .alias(currentAction.alias)
     .action((option, cmd) => {
       if (action === '*') {
-        console.log('\r\n ' + chalk.red(`${currentAction.description} ${chalk.yellow(cmd)}.`))
+        console.log(`\r\n ${chalk.red(`${currentAction.description} ${chalk.yellow(cmd)}.`)}`);
       } else if (action.includes('create')) {
         require('../lib/create')(option, cmd);
-      } else if(action.includes('config')) {
+      } else if (action.includes('config')) {
         require('../lib/config')(option, cmd);
       } else if (action.includes('init')) {
         require('../lib/init')(option, cmd);
       }
-    })
-})
+    });
+});
 
-function help(){
-  console.log(chalk.green('\r\n ' + `Run ${chalk.yellow(`daemon-cli <command> --help`)} for detailed usage of given command.`));
+function help() {
+  console.log(chalk.green('\r\n ' + `Run ${chalk.yellow('daemon-cli <command> --help')} for detailed usage of given command.`));
   console.log(chalk.magenta('\r\n ' + 'How to use command: '));
-  Object.keys(actionMap).forEach(action => {
-    actionMap[action].examples.forEach(example => {
-      console.log(chalk.cyan('  - ' + example))
-    })
-  })
+  Object.keys(actionMap).forEach((action) => {
+    actionMap[action].examples.forEach((example) => {
+      console.log(chalk.cyan(`  - ${example}`));
+    });
+  });
 }
 
 program.on('-h', help);
